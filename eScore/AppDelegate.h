68e4e15c0d3b7a711d7a3504ef37ca8525970f64
方壯雄 <Hsiung@houtsuyoshiyuutekiiMac.local>
2012-12-19 11:45:23 +0800

    add project file

---
+++ eScore/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  eScore
+//
+//  Created by 方壯雄 on 12/12/19.
+//  Copyright (c) 2012年 方壯雄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ eScore/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  eScore
+//
+//  Created by 方壯雄 on 12/12/19.
+//  Copyright (c) 2012年 方壯雄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
